f13fbca50bcffe814bbfb9853314d65490d99af5
AtefBN <ben.nasr.atef@outlook.fr>
2017-03-24 17:42:02 +0100

    updated with latest client version

--- media/js/viewer/data.parser.js
+++ media/js/viewer/data.parser.js
@@ -23,9 +23,9 @@
         console.log(data.issue)
         console.log(data.issue.experimentID)
         // Set issue full title.
-        issue._fullTitle = issue.project.toUpperCase();
+        issue._fullTitle = issue.mipEra.toUpperCase();
         issue._fullTitle += " - ";
-        issue._fullTitle += issue.institute.toUpperCase();
+        issue._fullTitle += issue.institutionID.toUpperCase();
         issue._fullTitle += " - ";
         issue._fullTitle += issue.title.slice(0, 48);
         if (issue.title.length > 48) {
@@ -33,8 +33,8 @@
         }
 
         // Reformat fields.
-        issue._institute = issue.institute.toUpperCase();
-        issue._project = issue.project.toUpperCase();
+        issue._institute = issue.institutionID.toUpperCase();
+        issue._project = issue.mipEra.toUpperCase();
         issue._experiments = issue.experimentID.length ?
                              issue.experimentID.sort().join(", ") : "--";
         issue._models = issue.sourceID.length ?
@@ -47,19 +47,19 @@
         issue._status = APP.state.status[issue.status];
 
         // Set documentation viewer links.
-        issue._projectDocURL = "https://documentation.es-doc.org/" + issue.project;
+        issue._projectDocURL = "https://documentation.es-doc.org/" + issue.mipEra;
         issue._experimentDocURLs = issue.experimentID.length === 0 ? [] :
             _.map(issue.experimentID.sort(), function (i) {
                 return {
                     label: i,
-                    hyperlink: "https://documentation.es-doc.org/" + issue.project + "/experiments/" + i
+                    hyperlink: "https://documentation.es-doc.org/" + issue.mipEra + "/experiments/" + i
                 };
             });
         issue._modelDocURLs = issue.sourceID.length === 0 ? [] :
             _.map(issue.sourceID.sort(), function (i) {
                 return {
                     label: i,
-                    hyperlink: "https://documentation.es-doc.org/" + issue.project + "/models/" + i
+                    hyperlink: "https://documentation.es-doc.org/" + issue.mipEra + "/models/" + i
                 };
             });
 
