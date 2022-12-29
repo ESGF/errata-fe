2cd2f6acea88267676f75aec62aeb0e5be6ad1d3
Mark A. Greenslade <asladeofgreen@pm.me>
2022-12-29 15:59:12 +0100

    1. URL constants updated.

--- media/js/shared/constants.urls.js
+++ media/js/shared/constants.urls.js
@@ -1,23 +1,20 @@
 // Set API base url.
 export const API_BASE_URL = window.origin;
 
-// Issue create.
+// Errata create.
 export const API_PUBLICATION_CREATE = "/2/errata/publication/create";
 
-// Issue retrieve.
+// Errata retrieve.
 export const API_PUBLICATION_RETRIEVE = "/2/errata/publication/retrieve";
 
-// Issue retrieve-all.
+// Errata retrieve-all.
 export const API_PUBLICATION_RETRIEVE_ALL = "/2/errata/publication/retrieve-all";
 
-// Issue update.
+// Errata update.
 export const API_PUBLICATION_UPDATE = "/2/errata/publication/update";
 
-// Issue moderation: propose.
-export const API_MODERATION_PROPOSE = "/2/errata/moderation/propose";
-
-// Issue moderation: search.
-export const API_MODERATION_SEARCH = "/2/errata/moderation/search";
+// Errata propose.
+export const API_MODERATION_PROPOSE = "/2/errata/publication/propose";
 
 // OAuth authorize.
 export const API_OAUTH_AUTHORIZE = window.location.origin + "/oauth/authorize";
@@ -25,36 +22,39 @@ export const API_OAUTH_AUTHORIZE = window.location.origin + "/oauth/authorize";
 // PID resolve.
 export const API_PID_RESOLVE = "/1/resolve/pid";
 
-// PID task queeu search.
+// PID task queue search.
 export const API_PID_TASK_QUEUE_SEARCH = "/1/pid-queue/search";
 
-// PID task queeu search setup.
+// PID task queue search setup.
 export const API_PID_TASK_QUEUE_SEARCH_SETUP = "/1/pid-queue/search-setup";
 
-// issue search.
+// Errata search.
 export const API_SEARCH = "/2/errata/search";
 
-// Issue search setup.
+// Errata search: moderation.
+export const API_SEARCH_MODERATION = "/2/errata/search/moderation";
+
+// Errata search setup.
 export const API_SEARCH_SETUP = "/2/errata/search/setup";
 
 // Set documentation url.
 export const DOCUMENTATION = "https://es-doc.github.io/esdoc-errata-client/";
 export const DOCUMENTATION_PID = "https://es-doc.github.io/esdoc-errata-client/lookup.html"
 
-// Issue moderation page.
+// Page: moderate errata.
 export const PAGE_MODERATE = '/static/moderate.html';
 
-// ES-DOC homepage.
+// Page: ES-DOC home.
 export const PAGE_HOME = "https://es-doc.org";
 
-// Set viewerBaseURL.
+// Page: Edit errata.
 export const PAGE_EDIT = '/static/edit.html';
 
-// PID page.
+// Page: PID.
 export const PAGE_PID = "pid.html";
 
-// Search page.
+// Page: Search errata.
 export const PAGE_SEARCH = "/static/index.html";
 
-// Set viewerBaseURL.
+// Page: View errata.
 export const PAGE_VIEW = '/static/view.html';
