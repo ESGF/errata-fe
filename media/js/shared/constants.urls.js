82dd24e396590d94ed15df5828c5fb86e90e6b7c
Mark A. Greenslade <asladeofgreen@pm.me>
2022-12-29 15:27:06 +0100

    1. URL constants updated.

--- media/js/shared/constants.urls.js
+++ media/js/shared/constants.urls.js
@@ -1,27 +1,23 @@
 // Set API base url.
 export const API_BASE_URL = window.origin;
 
-// Set documentation url.
-export const DOCUMENTATION = "https://es-doc.github.io/esdoc-errata-client/";
-export const DOCUMENTATION_PID = "https://es-doc.github.io/esdoc-errata-client/lookup.html"
-
 // Issue create.
-export const API_PUBLICATION_CREATE = "/2/issue/publication/create";
+export const API_PUBLICATION_CREATE = "/2/errata/publication/create";
 
 // Issue retrieve.
-export const API_PUBLICATION_RETRIEVE = "/2/issue/publication/retrieve";
+export const API_PUBLICATION_RETRIEVE = "/2/errata/publication/retrieve";
 
 // Issue retrieve-all.
-export const API_PUBLICATION_RETRIEVE_ALL = "/2/issue/publication/retrieve-all";
+export const API_PUBLICATION_RETRIEVE_ALL = "/2/errata/publication/retrieve-all";
 
 // Issue update.
-export const API_PUBLICATION_UPDATE = "/2/issue/publication/update";
+export const API_PUBLICATION_UPDATE = "/2/errata/publication/update";
 
 // Issue moderation: propose.
-export const API_MODERATION_PROPOSE = "/2/issue/moderation/propose";
+export const API_MODERATION_PROPOSE = "/2/errata/moderation/propose";
 
 // Issue moderation: search.
-export const API_MODERATION_SEARCH = "/2/issue/moderation/search";
+export const API_MODERATION_SEARCH = "/2/errata/moderation/search";
 
 // OAuth authorize.
 export const API_OAUTH_AUTHORIZE = window.location.origin + "/oauth/authorize";
@@ -36,10 +32,14 @@ export const API_PID_TASK_QUEUE_SEARCH = "/1/pid-queue/search";
 export const API_PID_TASK_QUEUE_SEARCH_SETUP = "/1/pid-queue/search-setup";
 
 // issue search.
-export const API_SEARCH = "/2/issue/search";
+export const API_SEARCH = "/2/errata/search";
 
 // Issue search setup.
-export const API_SEARCH_SETUP = "/2/issue/search-setup";
+export const API_SEARCH_SETUP = "/2/errata/search/setup";
+
+// Set documentation url.
+export const DOCUMENTATION = "https://es-doc.github.io/esdoc-errata-client/";
+export const DOCUMENTATION_PID = "https://es-doc.github.io/esdoc-errata-client/lookup.html"
 
 // Issue moderation page.
 export const PAGE_MODERATE = '/static/moderate.html';
