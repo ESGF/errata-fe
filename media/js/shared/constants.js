7d633c6bb700340427fd0ba0bce9094d9a5539ae
Mark A. Greenslade <asladeofgreen@pm.me>
2023-01-11 15:21:53 +0100

    1. Streamlined moderation button.

--- media/js/shared/constants.js
+++ media/js/shared/constants.js
@@ -1,11 +1,11 @@
-// Module imports.
 import * as _EMAIL from './constants.email.js';
+import * as _ISSUE from './constants.issue.js';
 import * as _MISC from './constants.misc.js';
 import * as _SECURITY from './constants.security.js';
 import * as _URLS from './constants.urls.js';
 
-// Module exports.
 export const EMAIL = _EMAIL;
+export const ISSUE = _ISSUE;
 export const MISC = _MISC;
 export const SECURITY = _SECURITY;
 export const URLS = _URLS;
